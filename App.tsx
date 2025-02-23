97c06f0cafc707167a9389d95b42b5cf0ff63067
daivd <daivuong7696@gmail.com>
2025-02-23 10:15:42 +0700

    Add theme and update App.tsx with initial styling

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { theme } from "./theme";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.itemContainer}>Item 1</Text>
     </View>
   );
 }
@@ -13,8 +12,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.colorWhite,
+    justifyContent: "center",
+  },
+  itemContainer: {
+    paddingVertical: 16,
+    paddingHorizontal: 8,
+    borderBottomColor: theme.colorCerulean,
+    borderBottomWidth: 1,
+  },
+  itemText: {
+    fontSize: 18,
+    fontWeight: "200",
   },
 });
